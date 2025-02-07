a290103fd07a6162996ee28f232b0c1074148837
Martin Höller <martin@martinhoeller.net>
2025-02-07 09:21:28 +0100

    fix build error in Mac version

--- OpenEmail/Mac/Profile/ReadersView.swift
+++ OpenEmail/Mac/Profile/ReadersView.swift
@@ -461,15 +461,3 @@ struct ReadersView: View {
     .frame(width: 500)
     .background(.themeViewBackground)
 }
-
-
-public extension View {
-    @ViewBuilder
-    func modify(@ViewBuilder _ transform: (Self) -> (some View)?) -> some View {
-        if let view = transform(self), !(view is EmptyView) {
-            view
-        } else {
-            self
-        }
-    }
-}
