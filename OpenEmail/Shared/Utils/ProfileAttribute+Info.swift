f86a68cb39bbd6044b5fb302fb5884a7032b79a7
kramo <git@kramo.page>
2025-03-13 12:28:28 +0100

    Fix typo

--- OpenEmail/Shared/Utils/ProfileAttribute+Info.swift
+++ OpenEmail/Shared/Utils/ProfileAttribute+Info.swift
@@ -9,7 +9,7 @@ extension ProfileAttribute {
         case .organization: return "The associated organization"
         case .jobTitle: return "Position or role description"
         case .publicAccess: return "When disabled, contact requests are automatically ignored."
-        case .publicLinks: return "When enabled, contacts in your address book is made public."
+        case .publicLinks: return "When enabled, contacts in your address book are made public."
         case .lastSeenPublic: return "When enabled, the time of your last activity is made public."
         case .addressExpansion: return "Allows replacing your own address with alternative destinations."
         default: return nil
