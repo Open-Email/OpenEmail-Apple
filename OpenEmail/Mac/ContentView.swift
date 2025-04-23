d3aeda4f18a1156d392f89baa992370c17bee652
Anton Akimchenko <akimchenko.antony@icloud.com>
2025-04-23 13:44:35 +0400

    fixed non-refreshable contact preview

--- OpenEmail/Mac/ContentView.swift
+++ OpenEmail/Mac/ContentView.swift
@@ -149,6 +149,7 @@ struct ContentView: View {
         if let selectedProfileViewModel, let selectedContactListItem {
             ProfileView(viewModel: selectedProfileViewModel, isContactRequest: selectedContactListItem.isContactRequest)
                 .frame(minWidth: 600)
+                .id(selectedProfileViewModel.emailAddress.address)
         } else {
             Text("No selection")
                 .bold()
