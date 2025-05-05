b000f476b8c02c10980aa12366938244089e22fe
Anton Akimchenko <akimchenko.antony@icloud.com>
2025-05-05 17:48:43 +0400

    unwrapped info buttons with hints

--- OpenEmail/Mac/Profile/ProfileEditor/Group Views/ConfigurationProfileAttributesEditorView.swift
+++ OpenEmail/Mac/Profile/ProfileEditor/Group Views/ConfigurationProfileAttributesEditorView.swift
@@ -10,44 +10,17 @@ struct ConfigurationProfileAttributesEditorView: View {
             VStack(alignment: .leading, spacing: .Spacing.default) {
                 Text("Configuration").font(.title2)
                 
-                HStack {
-                    Toggle("", isOn: Binding($profile)?.publicAccess ?? Binding<Bool>(
-                        get: { true
-                        },
-                        set: {_ in }))
-                    Text(ProfileAttribute.publicAccess.displayTitle)
-                    
-                    if let info = ProfileAttribute.publicAccess.info {
-                        InfoButton(text: info)
-                    }
-                }
-                HStack {
-                    Toggle("", isOn: Binding($profile)?.publicLinks ?? Binding<Bool>(
-                        get: { true },
-                        set: {_ in }))
-                    Text(ProfileAttribute.publicLinks.displayTitle)
-                    
-                    if let info = ProfileAttribute.publicLinks.info {
-                        InfoButton(text: info)
-                    }
-                }
-                HStack {
-                    Toggle("", isOn: Binding($profile)?.lastSeenPublic ?? Binding<Bool>(
-                        get: { true },
-                        set: {_ in }))
-                    Text(ProfileAttribute.lastSeenPublic.displayTitle)
-                    
-                    if let info = ProfileAttribute.lastSeenPublic.info {
-                        InfoButton(text: info)
-                    }
-                }
+                getConfigurationToggleView(attribute: ProfileAttribute.publicAccess, isOn: Binding($profile)?.publicAccess)
+                getConfigurationToggleView(attribute: ProfileAttribute.publicLinks, isOn: Binding($profile)?.publicLinks)
+                getConfigurationToggleView(attribute: ProfileAttribute.lastSeenPublic, isOn: Binding($profile)?.lastSeenPublic)
                 
-                VStack(alignment: .leading, spacing: .Spacing.xSmall) {
-                    HStack {
-                        OpenEmailTextFieldLabel(ProfileAttribute.addressExpansion.displayTitle)
-                        if let info = ProfileAttribute.addressExpansion.info {
-                            InfoButton(text: info)
-                        }
+                VStack(alignment: .leading) {
+                    Text(ProfileAttribute.addressExpansion.displayTitle)
+                    if let info = ProfileAttribute.addressExpansion.info {
+                        Text(info)
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline)
+                            .truncationMode(.tail)
                     }
                     
                     TextField("Enter addresses", text: Binding($profile)?.addressExpansion ?? Binding<String>(
@@ -63,7 +36,28 @@ struct ConfigurationProfileAttributesEditorView: View {
             .frame(maxHeight: .infinity, alignment: .top)
         }
     }
+    
+    private func getConfigurationToggleView(attribute: ProfileAttribute, isOn: Binding<Bool>?) -> some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                Text(attribute.displayTitle)
+                if let info = attribute.info {
+                    Text(info)
+                        .foregroundStyle(.secondary)
+                        .font(.subheadline)
+                        .truncationMode(.tail)
+                }
+            }
+            Spacer()
+            Toggle("", isOn: isOn ?? Binding<Bool>(
+                get: { true
+                },
+                set: {_ in }))
+        }
+    }
 }
+
+
 
 #Preview {
     @Previewable @State var profile: Profile? = .makeFake()
--- OpenEmail/Shared/Utils/ProfileAttribute+Info.swift
+++ OpenEmail/Shared/Utils/ProfileAttribute+Info.swift
@@ -5,8 +5,6 @@ import OpenEmailCore
 extension ProfileAttribute {
     var info: String? {
         switch self {
-        case .organization: return "The associated organization"
-        case .jobTitle: return "Position or role description"
         case .publicAccess: return "When disabled, contact requests are automatically ignored."
         case .publicLinks: return "When enabled, contacts in your address book are made public."
         case .lastSeenPublic: return "When enabled, the time of your last activity is made public."
